d5a8ab22cb300db8ab2983bccd112e86a4ddfb9c
evan <evan@sdicg.com>
2016-02-10 21:00:52 -0500

    adding context to the find signature

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const middleware = (Router) => async (ctx, next) => {
   ctx.body = await Router
-                    .find(ctx.method, ctx.path, ctx.request.body, ctx.headers)
+                    .find(ctx.method, ctx.path, ctx.request.body, ctx.headers, ctx)
                     .catch((err) => {
                       console.log(err.stack);
                       ctx.status = err.code||ctx.status;

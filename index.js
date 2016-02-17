93785ed248ab55b88969ecf80391e3498d915ea2
evan <evan@sdicg.com>
2016-02-16 21:08:27 -0500

    outputting err if stack doesn't exist

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const middleware = (Router) => async (ctx, next) => {
   ctx.body = await Router
                     .find(ctx.method, ctx.path, ctx.request.body, ctx.headers, ctx)
                     .catch((err) => {
-                      console.log(err.stack);
+                      console.log(err.stack||err);
                       ctx.status = err.code||ctx.status;
                       return err;
                     });

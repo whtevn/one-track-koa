5ae86498de20f433d2966fde2a999d6f0fe830b7
Deac <deac@sdicg.com>
2016-04-01 15:16:04 -0400

    added require for babel-polyfill

--- build/index.js
+++ build/index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
 
+require("babel-polyfill");
 var middleware = function middleware(Router) {
   return function () {
     var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(ctx, next) {
@@ -29,11 +30,10 @@ var middleware = function middleware(Router) {
           }
         }
       }, _callee, undefined);
-    })),
-        _this = undefined;
+    }));
 
     return function (_x, _x2) {
-      return ref.apply(_this, arguments);
+      return ref.apply(this, arguments);
     };
   }();
 };

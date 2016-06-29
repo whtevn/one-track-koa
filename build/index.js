bd8daae217d0a67326a35f56b6e4828bd11b2a1c
Evan Short <evan.short@gmail.com>
2016-06-29 14:32:43 -0400

    updating build

--- build/index.js
+++ build/index.js
@@ -9,13 +9,13 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
 require("babel-polyfill");
 var middleware = function middleware(Router) {
   return function () {
-    var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(ctx, next) {
+    var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(ctx, next) {
       return regeneratorRuntime.wrap(function _callee$(_context) {
         while (1) {
           switch (_context.prev = _context.next) {
             case 0:
               _context.next = 2;
-              return Router.find(ctx.method, ctx.path, ctx.headers, ctx.request.body, ctx).catch(function (err) {
+              return Router.find(ctx.method, ctx.path, ctx.headers, ctx.request.body, ctx.query, ctx).catch(function (err) {
                 console.log(err.stack || err);
                 ctx.status = err.code || ctx.status;
                 return err;
@@ -33,7 +33,7 @@ var middleware = function middleware(Router) {
     }));
 
     return function (_x, _x2) {
-      return ref.apply(this, arguments);
+      return _ref.apply(this, arguments);
     };
   }();
 };

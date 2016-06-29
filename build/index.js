b710c615d8b4bb8458e5c75e35ee96d87d01eb04
Evan Short <evan.short@gmail.com>
2016-06-29 14:54:42 -0400

    parse query string items

--- build/index.js
+++ build/index.js
@@ -10,21 +10,25 @@ require("babel-polyfill");
 var middleware = function middleware(Router) {
   return function () {
     var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(ctx, next) {
+      var query;
       return regeneratorRuntime.wrap(function _callee$(_context) {
         while (1) {
           switch (_context.prev = _context.next) {
             case 0:
-              _context.next = 2;
-              return Router.find(ctx.method, ctx.path, ctx.headers, ctx.request.body, ctx.query, ctx).catch(function (err) {
+              query = ctx.query.map(function (q) {
+                return JSON.parse(q);
+              });
+              _context.next = 3;
+              return Router.find(ctx.method, ctx.path, ctx.headers, ctx.request.body, query, ctx).catch(function (err) {
                 console.log(err.stack || err);
                 ctx.status = err.code || ctx.status;
                 return err;
               });
 
-            case 2:
+            case 3:
               ctx.body = _context.sent;
 
-            case 3:
+            case 4:
             case "end":
               return _context.stop();
           }
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require("babel-polyfill");
 const middleware = (Router) => async (ctx, next) => {
+  const query = ctx.query.map(q => JSON.parse(q));
   ctx.body = await Router
-                    .find(ctx.method, ctx.path, ctx.headers, ctx.request.body, ctx.query, ctx)
+                    .find(ctx.method, ctx.path, ctx.headers, ctx.request.body, query, ctx)
                     .catch((err) => {
                       console.log(err.stack||err);
                       ctx.status = err.code||ctx.status;
